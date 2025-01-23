0cb477c70643cfc13ede7bb0dffcd923f7eabe73
fonsirs <fonsirs@gmail.com>
2025-01-23 01:46:44 +0100

    add logger to closeJobPost

--- src/jobPost/application/closeJobPost.ts
+++ src/jobPost/application/closeJobPost.ts
@@ -4,10 +4,11 @@ import {
     closeJobPost as closeJobPostEntity,
 } from 'jobPost/domain/jobPost';
 import { ScrappedJobPost } from 'company/infrastructure/scrapping/companyScrapper';
+import { logger } from 'shared/infrastructure/logger/logger';
 
-type CreateJobPostCommand = JobPost | ScrappedJobPost;
+type CloseJobPostCommand = JobPost | ScrappedJobPost;
 
-export const closeJobPost = async (command: CreateJobPostCommand) => {
+export const closeJobPost = async (command: CloseJobPostCommand) => {
     const { originalId, companyId } = command;
     const jobPost = await jobPostRepository.getByOriginalIdAndCompanyId(
         originalId,
@@ -15,6 +16,10 @@ export const closeJobPost = async (command: CreateJobPostCommand) => {
     );
 
     if (!jobPost || jobPost.closedAt) {
+        logger.error(new Error('Error closing job post'), {
+            originalId,
+            companyId,
+        });
         return;
     }
 
